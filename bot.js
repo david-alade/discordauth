47724e90e8a18afcce587711f038c70f0b1905ee
David Alade <davidalade1051@gmail.com>
2020-09-07 17:29:35 -0400

    transfer

---
+++ bot.js
@@ -0,0 +1,7 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+    console.log('Ready!');
+  });
+  client.login('NzE5OTYwODEwODY4NDQxMTI5.XubvMw.aQ-NN8qcuskUUC7tJJKiI90kieA');
