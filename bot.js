e3898b16abad92e438d3719e028ca6795a5273d5
david alade <45858773+david-alade@users.noreply.github.com>
2020-09-07 17:32:12 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,4 +4,4 @@ const client = new Discord.Client();
 client.once('ready', () => {
     console.log('Ready!');
   });
-  client.login('NzE5OTYwODEwODY4NDQxMTI5.XubvMw.aQ-NN8qcuskUUC7tJJKiI90kieA');
+  client.login('xxxxxx');
